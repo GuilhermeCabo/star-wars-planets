c43279e864812ca7d65a26e8ec5b18743006804e
Guilherme Cabo <guilherme.pcabo@gmail.com>
2020-05-25 09:39:28 -0300

    Changed API url

--- src/App.js
+++ src/App.js
@@ -1,116 +1,117 @@
-import React, { Component } from 'react';
-import { PlanetInfoBox, PlanetNameBox, PlanetName, PlanetDetailsBox, PlanetDetail,
-         Button
-      } from './components/styled'
-import api from './services/api'
-import { ZoomyDiv } from './components/animations'
-import './App.css';
+import React, { useState, useEffect } from "react";
+import {
+  PlanetInfoBox,
+  PlanetNameBox,
+  PlanetName,
+  PlanetDetailsBox,
+  PlanetDetail,
+  Button
+} from "./components/styled";
+import api from "./services/api";
+import { ZoomyDiv } from "./components/animations";
+import "./App.css";
 
-export default class App extends Component {
-  componentDidMount() {
-    this.handlePlanet()
-  }
+export default function App() {
+  const [planet, setPlanet] = useState(null);
+  const [films, setFilms] = useState([]);
+  const [err, setErr] = useState(null);
+  const [loading, setLoading] = useState(null);
 
-  constructor(props) {
-    super(props)
+  useEffect(() => {
+    async function handlePlanet() {
+      try {
+        const random = Math.random() * 61 + 1;
+        const { data } = await api.get(`planets/${random.toFixed(0)}`);
+        this.setState({ planet: data, loading: false });
 
-    this.state = {
-      planet: null,
-      films: [],
-      err: null,
-      loading: true
+        setPlanet(data);
+        setLoading(false);
+
+        Promise.all(
+          data.films.map(async filmUrl => await handleFilms(filmUrl))
+        );
+      } catch (err) {
+        setLoading(false);
+        setErr(
+          `Our robots crashed while exploring the planet. Please, try again`
+        );
+      }
     }
-  }
 
-  async handlePlanet() {
-    this.setState( { planet: null, loading: true, err: null, films: [] } )
-    try {
-      const random = (Math.random() * 61) + 1
-      const { data } = await api.get(`planets/${random.toFixed(0)}`)
-      this.setState( { planet: data, loading: false } )
-      data.films.forEach((filmUrl) => { this.handleFilms(filmUrl) })
-    } catch (err) {
-      this.setState(
-        { 
-          loading: false,
-          planet: null,
-          films: [],
-          err: `Our robots crashed while exploring the planet. Please, try again`
-        }
-      )
+    async function handleFilms(filmUrl) {
+      const { data } = await api.get(
+        filmUrl.replace("https://swapi.co/api/", "")
+      );
+      const { title } = data;
+      setFilms([...films, title]);
+      setLoading(false);
     }
-  }
-  
-  async handleFilms(filmUrl) {
-    const { data } = await api.get(filmUrl.replace('https://swapi.co/api/', ''))
-    const { title } = data
-    this.setState(prevState => ({
-      films: [...prevState.films, title],
-      loading: false
-    }))
-  }
 
-  render() {
-    const { planet, films, loading, err } = this.state
-    return (
-      <div className='App'>
-        <img
-          src='https://starwarsblog.starwars.com/wp-content/uploads/2015/10/Seastrom_HeaderPhoto-2400x1200-900792609620.jpg'
-          className='AppBackground'
-          alt='stars-background'
-        />
-        {loading && (
-          <PlanetInfoBox backgroundColor='#01242d'>
-            <img 
-              src='https://i.gifer.com/SxF.gif' 
-              className='Loading' 
-              alt='bb8-loading'
+    handlePlanet();
+  }, []);
+
+  return (
+    <div className="App">
+      <img
+        src="https://starwarsblog.starwars.com/wp-content/uploads/2015/10/Seastrom_HeaderPhoto-2400x1200-900792609620.jpg"
+        className="AppBackground"
+        alt="stars-background"
+      />
+      {loading && (
+        <PlanetInfoBox backgroundColor="#01242d">
+          <img
+            src="https://i.gifer.com/SxF.gif"
+            className="Loading"
+            alt="bb8-loading"
+          />
+        </PlanetInfoBox>
+      )}
+
+      {!!err && (
+        <ZoomyDiv animation="1s">
+          <PlanetInfoBox padding="5vw">
+            <img
+              src={require("./images/r2d2.png")}
+              className="ErrorImage"
+              alt="error"
             />
+            <h1 className="Error">{err}</h1>
+            <Button onClick={() => handlePlanet()}> Try Again </Button>
           </PlanetInfoBox>
-        )}
+        </ZoomyDiv>
+      )}
 
-        {!!err && (
-          <ZoomyDiv animation='1s'>
-            <PlanetInfoBox padding='5vw'>
-              <img src={require('./images/r2d2.png')} className='ErrorImage' alt='error'/>
-              <h1 className='Error'>{err}</h1>
-              <Button onClick={() => this.handlePlanet()}> Try Again </Button>
-            </PlanetInfoBox>
-          </ZoomyDiv>
-        )}
-
-        {!!planet && (
-          <ZoomyDiv>
-            <PlanetInfoBox>
-              <PlanetNameBox>
-                <PlanetName>{planet.name}</PlanetName>
-              </PlanetNameBox>
-              <PlanetDetailsBox>
-                <PlanetDetail>
-                  <strong className='CustomFont'>{`PoPuLATioN: `}</strong>
-                  { planet.population !== 'unknown' ? 
-                  parseInt(planet.population).toLocaleString()
-                  : planet.population }
-                </PlanetDetail>
-                <PlanetDetail>
-                  <strong className='CustomFont'>{`CLiMATE: `}</strong>
-                  {planet.climate}
-                </PlanetDetail>
-                <PlanetDetail>
-                <strong className='CustomFont'>{`TERRAiN: `}</strong>
+      {!!planet && (
+        <ZoomyDiv>
+          <PlanetInfoBox>
+            <PlanetNameBox>
+              <PlanetName>{planet.name}</PlanetName>
+            </PlanetNameBox>
+            <PlanetDetailsBox>
+              <PlanetDetail>
+                <strong className="CustomFont">{`PoPuLATioN: `}</strong>
+                {planet.population !== "unknown"
+                  ? parseInt(planet.population).toLocaleString()
+                  : planet.population}
+              </PlanetDetail>
+              <PlanetDetail>
+                <strong className="CustomFont">{`CLiMATE: `}</strong>
+                {planet.climate}
+              </PlanetDetail>
+              <PlanetDetail>
+                <strong className="CustomFont">{`TERRAiN: `}</strong>
                 {planet.terrain}
-                </PlanetDetail>
-                <PlanetDetail>
-                  <strong className='CustomFont'>{`FEATURED iN ${films.length} 
-                  ${films.length === 1 ? 'FiLM' : 'FiLMS'}`}</strong>
-                </PlanetDetail>
-              </PlanetDetailsBox>
+              </PlanetDetail>
+              <PlanetDetail>
+                <strong className="CustomFont">{`FEATURED iN ${films.length} 
+                  ${films.length === 1 ? "FiLM" : "FiLMS"}`}</strong>
+              </PlanetDetail>
+            </PlanetDetailsBox>
 
-              <Button onClick={() => this.handlePlanet()}> NEW PLANET </Button>
-            </PlanetInfoBox>
-          </ZoomyDiv>
-        )}
-      </div>
-    );
-  }
+            <Button onClick={() => this.handlePlanet()}> NEW PLANET </Button>
+          </PlanetInfoBox>
+        </ZoomyDiv>
+      )}
+    </div>
+  );
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'https://swapi.co/api/',
+  baseURL: 'https://swapi.dev/api/',
   headers: {
     'Content-Type': 'application/json',
   }
